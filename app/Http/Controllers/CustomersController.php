4cf4147831de82473aa13ec5400d720249e12b51
Laciann <abhishek.acharya@apexcollege.edu.np>
2019-06-18 03:04:08 +0545

    refactored code

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 class CustomersController extends Controller
 {
     public function list(){
-        $activeCustomers = Customer::where('active',1)->get();
-        $inactiveCustomers = Customer::where('active',0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
         return view('internals.customers',compact('activeCustomers','inactiveCustomers'));
     }
@@ -17,16 +17,20 @@ class CustomersController extends Controller
     public function store(){
        // dd(request('name'));
        //validation
+
        $data = request()->validate([
            'name' => 'required|min:3',
             'email' => 'required|email',
             'active' => 'required',
        ]);
-       $customer = new Customer();
-       $customer->name = request('name');
-        $customer->email = request('email');
-        $customer->active = request('active');
-       $customer->save();
+
+       Customer::create($data);
+
+    //    $customer = new Customer();
+    //    $customer->name = request('name');
+    //     $customer->email = request('email');
+    //     $customer->active = request('active');
+    //    $customer->save();
 
        return back(); //return back to the view
     }
